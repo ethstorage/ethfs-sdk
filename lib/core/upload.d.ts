833a2e8879135715ef81a30189a8547173bd8b05
iteyelmp@gmail.com <lmp19890127>
2023-02-22 18:57:55 +0800

    fix build

--- lib/core/upload.d.ts
+++ lib/core/upload.d.ts
@@ -2,5 +2,5 @@
 import { Signer } from "ethers";
 export declare const FileAbi: string[];
 export declare const FlatDirectoryAbi: string[];
-export declare function upload(signer: Signer, address: string, fileName: string, fileSize: number, fileData: Buffer, dirPath?: string, onProgress?: Function, onSuccess?: Function, onError?: Function): Promise<void>;
+export declare function upload(signer: Signer, address: string, directoryPath: string, fileSize: number, fileData: Buffer, onProgress?: Function, onSuccess?: Function, onError?: Function): Promise<void>;
 export declare function createDirectory(signer: Signer): Promise<string | false>;
